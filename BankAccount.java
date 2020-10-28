c8376961e6e0a91bcb1b91831f0f15927b758936
David Chong <djchong24@gmail.com>
2020-10-27 20:07:56 -0400

    finished constructor

--- BankAccount.java
+++ BankAccount.java
@@ -1,3 +1,17 @@
+import java.nio.channels.AcceptPendingException;
+
 public class BankAccount{
+    //Instance Variables
+    private String password;
+    private double balance;
+    private int accountID;
+
+    public BankAccount(String a, int b){
+        password = a;
+        accountID = b;
+        balance = 0;
+    }
+    
+    
     
 }
